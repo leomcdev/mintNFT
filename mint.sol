f1224b43e0931966c18882d6116e625d8feb7f4b
Leo B Mcneil <74776992+leomcdev@users.noreply.github.com>
2022-08-01 12:09:36 +0200

    Update mint.sol

--- mint.sol
+++ mint.sol
@@ -70,12 +70,3 @@ function tokenURI(uint256 _tokenId) public view virtual override returns(string 
     );
     return _tokenURIs[_tokenId];
 }
-    /// @notice Enumerate valid NFTs
-    /// @dev Throws if `_index` >= `totalSupply()`.
-    /// @param _index A counter less than `totalSupply()`
-    /// @return The token identifier for the `_index`th NFT,
-    ///  (sort order not specified)
-    function tokenByIndex(uint256 _index) external view returns (uint256) {
-        //needed to validate
-    }
-}
